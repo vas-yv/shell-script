395e2b566d2f8201134864351006dba306e0e395
vasuy <vasuyaragorla7@gmail.com>
2025-03-26 13:50:40 +0530

    adding variables

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "vas:: Hello yash, How are you?"
+echo "yash:: vas, I am fine. How are you?"
+echo "vas:: I am fine too. how is your work?"
+echo "yash:: not bad. I am thinking to upgrade to devops"
