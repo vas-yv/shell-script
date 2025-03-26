bb48717e6683c66de2c26816d78dd4d3dc9fa956
vasuy <vasuyaragorla7@gmail.com>
2025-03-26 10:53:06 +0530

    commiting changes

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,2 +1,8 @@
 create repo in githud
 clone repo into your local machine.
+
+imp cmds for adding file and commiting and pushing
+
+1. git add <file-name>
+2. git commit -m <reason for commiting>
+3. git push origin main
