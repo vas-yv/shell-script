0e5ff385bed63623543caf55c77ac1f93008e27c
vasuy <vasuyaragorla7@gmail.com>
2025-03-26 17:50:01 +0530

    installations

--- 11-installations.sh
+++ 11-installations.sh
@@ -5,5 +5,10 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo " please run this script with root access."
+else
+    echo " super user"
 fi    
+
 dnf install mysql -y
+
+echo "is script proceeding?"
