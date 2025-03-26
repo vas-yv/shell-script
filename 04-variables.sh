ade2e7378884ace660eb3a64aea721c1729e2297
vasuy <vasuyaragorla7@gmail.com>
2025-03-26 14:07:19 +0530

    adding variables

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person=$1
+person=$2
+
+echo "$person1:: hello $person2, how are you?"
+echo "$person2:: hi $person1 fine, how are you?"
+echo "$person1:: i am fine too. how is your work?"
+echo "$person2:: i am thinking to upgrade to devops"
