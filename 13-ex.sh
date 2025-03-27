ab6c06897c0a7ee0905ac0d58c514e28a843c89f
vasuy <vasuyaragorla7@gmail.com>
2025-03-27 19:11:24 +0530

    ex

--- 13-ex.sh
+++ 13-ex.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(basename "$0" | cut -d "." -f1)
-LOGFILE="/tmp/$SCRIPT_NAME-$TIMESTAMP.log"
+userid=$(id -u)
 
-# Debugging
-echo "USERID: $USERID"
-echo "TIMESTAMP: $TIMESTAMP"
-echo "SCRIPT_NAME: $SCRIPT_NAME"
-echo "LOGFILE: $LOGFILE"
-
-# Create log file
-touch "$LOGFILE"
-
-# Check if log file is created
-if [ -f "$LOGFILE" ]; then
-    echo "Log file created successfully at $LOGFILE"
-else
-    echo "Failed to create log file."
-fi
+dnf install git -y
