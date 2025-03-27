58433d709cd0e9f077e78fbe65069abf47dd0aaf
vasuy <vasuyaragorla7@gmail.com>
2025-03-27 18:56:14 +0530

    functions

--- 12-functions.sh
+++ 12-functions.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/vas/$SCRIPT_NAME-$TIMESTAMP.log
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -23,9 +23,9 @@ else
     echo "super user"
 fi    
 
-dnf install mysql -y
+dnf install mysql -y &>>LOGFILE
 VALIDATE $? "installing mysql"
 
 
-dnf install git -y
+dnf install git -y &>>LOGFILE
 VALIDATE $? "installing git"
