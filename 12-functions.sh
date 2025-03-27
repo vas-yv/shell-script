6dcb77b73cfc3fdb51df586ec784c5f62479ae93
vasuy <vasuyaragorla7@gmail.com>
2025-03-27 17:37:36 +0530

    functions

--- 12-functions.sh
+++ 12-functions.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
