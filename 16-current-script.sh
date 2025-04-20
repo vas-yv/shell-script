d1c3f0f727710f978262e137e4ec97c0cfddc9cc
vasuy <vasuyaragorla7@gmail.com>
2025-04-20 11:38:22 +0530

    shell

---
+++ 16-current-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+COURSE="Devops from Current Script"
+
+echo "Before calling other script, course: $COURSE"
+echo "Process ID of current shell script: $$"
+
+./17-other-script.sh
+
+echo "After calling other script, course: $COURSE"
